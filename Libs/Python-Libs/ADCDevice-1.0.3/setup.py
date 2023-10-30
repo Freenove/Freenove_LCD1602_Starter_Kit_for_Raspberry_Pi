9b65ab75899811e0311d8c75457b1e279bc1f3a3
PJB123456 <55983097+PJB123456@users.noreply.github.com>
2023-10-30 11:21:20 +0800

    Update

---
+++ Libs/Python-Libs/ADCDevice-1.0.3/setup.py
@@ -0,0 +1,19 @@
+
+#from setuptools import setup,find_packages
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "ADCDevice",
+    version = "V1.0.3",
+    description = "Freenove ADC Module python library.",
+    author = "Freenove",
+    url = "http://www.freenove.com",
+    license = " ",
+    packages = find_packages('src'),
+    package_dir = {'':'src'},
+    include_package_data = True,
+    #scripts = ["ADCDevice.py"],
+    
+    )
